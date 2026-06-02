64ecf5638d05f8d51916c990a26542075e5f5904
Thomas Anderson <roocifer246@gmail.com>
2026-06-02 11:51:30 -0600

    adding db context, connection strings and wiring up postgres

---
+++ backend/Mise/Mise.Infrastructure/Persistence/Context/MiseDbContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mise.Infrastructure.Persistence.Context
+{
+	public class MiseDbContext : DbContext
+	{
+
+        public MiseDbContext(DbContextOptions<MiseDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MiseDbContext).Assembly);
+        }
+    }
+}
