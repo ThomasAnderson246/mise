29e30a4b10d38acc7bd55b8987d4d4990f61da8a
Thomas Anderson <roocifer246@gmail.com>
2026-06-02 10:30:31 -0600

    scaffold vite frontend, added tailwind and pwa

---
+++ frontend/mise-web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <h1 className="text-2x1 font-bold text-[#6B1A2B]">Mise</h1>
+    </div>
+  )
+}
+
+export default App
